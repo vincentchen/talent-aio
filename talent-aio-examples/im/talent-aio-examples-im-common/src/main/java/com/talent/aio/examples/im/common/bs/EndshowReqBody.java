af53de6337b8f66401f2911e47c75da67fd4402c
tywo45 <tywo45@163.com>
2016-12-19 12:49:46 +0800

    去掉getter工具

--- talent-aio-examples/im/talent-aio-examples-im-common/src/main/java/com/talent/aio/examples/im/common/bs/EndshowReqBody.java
+++ talent-aio-examples/im/talent-aio-examples-im-common/src/main/java/com/talent/aio/examples/im/common/bs/EndshowReqBody.java
@@ -6,11 +6,7 @@ package com.talent.aio.examples.im.common.bs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 //@lombok.AllArgsConstructor
 public class EndshowReqBody extends BaseReqBody
 {
--- talent-aio-examples/im/talent-aio-examples-im-common/src/main/java/com/talent/aio/examples/im/common/bs/JoinNotifyRespBody.java
+++ talent-aio-examples/im/talent-aio-examples-im-common/src/main/java/com/talent/aio/examples/im/common/bs/JoinNotifyRespBody.java
@@ -3,9 +3,6 @@
  */
 package com.talent.aio.examples.im.common.bs;
 
-import lombok.Getter;
-import lombok.Setter;
-
 /**
  * 
  * @author tanyaowu 
@@ -15,9 +12,7 @@ import lombok.Setter;
  *  编号	| 操作时间	| 操作人员	 | 操作说明
  *  (1) | 2013年8月10日 | tanyaowu | 新建类
  */
-@Getter
-@Setter
-@lombok.AllArgsConstructor
+
 public class JoinNotifyRespBody extends com.talent.aio.examples.im.common.bs.BaseRespBody
 {
 	private String group;
