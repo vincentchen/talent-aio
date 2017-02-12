18603b0b4d07824ab8cdfa531367f9fb4f54207b
talent-tan <tywo45@163.com>
2017-02-12 17:15:18 +0800

    no commit message

--- example/im/talent-aio-examples-im-server/dist.bat
+++ example/im/talent-aio-examples-im-server/dist.bat
@@ -3,5 +3,3 @@ call mvn -f pom-dist.xml clean install
 rd ..\..\..\dist\talent-aio-examples-im-server-1.0.1.20170119-SNAPSHOT /s /q
 
 copy target\talent-aio-examples-im-server-1.0.1.20170119-SNAPSHOT.zip ..\..\..\dist\talent-aio-examples-im-server-1.0.1.20170119-SNAPSHOT.zip
-
-pause
